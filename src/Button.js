1aa728ed60383fce3426a3cb536645b19f97442a
TamaghnaGhosh <ghoshtamaghna6991@gmail.com>
2023-07-30 13:23:26 +0530

    package updated

--- src/Button.js
+++ src/Button.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const Button = ({ onClick, children }) => {
-    return <button onClick={onClick}>{children}</button>;
+    const buttonStyle = {
+        backgroundColor: '#007bff',
+        color: '#fff',
+        padding: '10px 20px',
+        border: 'none',
+        borderRadius: '4px',
+        cursor: 'pointer',
+    };
+    return <button onClick={onClick} style={buttonStyle}>{children}</button>;
 };
 
 export default Button;
